ec7babf643ed6fac20b503288f3d5269c477eac7
contity <yu.2.tao@continental-corporation.com>
2020-02-02 12:35:18 +0800

    correct types

--- Software/mc/src/User/Framework/Common/types.h
+++ Software/mc/src/User/Framework/Common/types.h
@@ -8,11 +8,11 @@
 #ifndef FRAMEWORK_COMMON_TYPES_H_
 #define FRAMEWORK_COMMON_TYPES_H_
 
-typedef unsigned char uint8;
-typedef unsigned char uint16;
-typedef unsigned char uint32;
-typedef   signed char sint8;
-typedef   signed char sint16;
-typedef   signed char sint32;
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned long  uint32;
+typedef   signed char  sint8;
+typedef   signed short sint16;
+typedef   signed long  sint32;
 
 #endif /* FRAMEWORK_COMMON_TYPES_H_ */
