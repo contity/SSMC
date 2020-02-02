167ae9d68a9f34f9e4b9b4b4a566257b1903c3e1
contity <yu.2.tao@continental-corporation.com>
2020-02-02 12:38:20 +0800

    #3 initial touch key files

---
+++ Software/mc/src/User/BasicSoftware/TouchKey/touchkey.c
@@ -0,0 +1,18 @@
+/*
+ * touchkey.c
+ *
+ *  Created on: Feb 1, 2020
+ *      Author: contity
+ */
+
+#include "touchkey.h"
+#include <stdio.h>
+#include "BasicSoftware/SystemTime/systemtime.h"
+
+void tkey_init(void)
+{
+}
+
+void tkey_task(void)
+{
+}
---
+++ Software/mc/src/User/BasicSoftware/TouchKey/touchkey.h
@@ -0,0 +1,14 @@
+/*
+ * touchkey.h
+ *
+ *  Created on: Feb 1, 2020
+ *      Author: contity
+ */
+
+#ifndef APPLICATIONSOFTWARE_TOUCHKEY_TOUCHKEY_H_
+#define APPLICATIONSOFTWARE_TOUCHKEY_TOUCHKEY_H_
+
+extern void tkey_init(void);
+extern void tkey_task(void);
+
+#endif /* APPLICATIONSOFTWARE_TOUCHKEY_TOUCHKEY_H_ */
