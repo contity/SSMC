2958b1bbe5b4077589722ff48050448e1a92cecc
contity <yu.2.tao@continental-corporation.com>
2020-02-04 00:52:44 +0800

    rotatelamp ongoing

--- Software/mc/src/User/ApplicationSoftware/RotateLamp/rotatelamp.c
+++ Software/mc/src/User/ApplicationSoftware/RotateLamp/rotatelamp.c
@@ -9,6 +9,62 @@
 #include "Framework/Common/types.h"
 #include "gpio.h"
 
+/* =============================================================================== */
+/* simulated rotator ======================================================= begin */
+
+typedef enum
+{
+	SIRO_DIR_STOP,
+	SIRO_DIR_CW,
+	SIRO_DIR_CCW,
+}
+SIRO_DIR;
+
+typedef struct
+{
+	/* input */
+	uint8 powerPlus;
+	uint8 powerMinus;
+	/* process */
+	sint16 acc;       // 1 pos/ms /ms
+	sint16 speed;     // 1 pos/ms
+	sint16 position;  // 256 pos/cyce
+
+	SIRO_DIR dir;
+	uint8 position; // 256 position / cycle
+	uint8 speed;    // 1 position / ms
+	/* output */
+	uint8 out;
+}
+siro_t;
+
+siro_t siro;
+
+static void siro_init(void)
+{
+	siro.powerMinus = 0u;
+	siro.powerPlus = 0u;
+	siro.dir = SIRO_DIR_STOP;
+	siro.out = 0u;
+}
+
+static void siro_task1ms(void)
+{
+	/* input */
+
+	/* process - dir */
+	if(        siro.powerPlus && !siro.powerMinus ) siro.dir = SIRO_DIR_CW;
+	else if ( !siro.powerPlus &&  siro.powerMinus ) siro.dir = SIRO_DIR_CCW;
+	else                                            siro.dir = SIRO_DIR_STOP;
+
+	/* process - rotate */
+
+	/* output */
+}
+
+/* simulated rotator ========================================================= end */
+/* =============================================================================== */
+
 typedef struct
 {
 	uint8 out;
